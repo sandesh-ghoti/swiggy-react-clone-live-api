23e71d1da73ae6f9e4e80856d7b9fe09f302301d
sandesh ghoti <sandesh.ghot@gmail.com>
2024-01-28 09:42:51 +0000

    parcel-react-tailwind template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hello from "./components/Hello";
+
+const App = () => {
+  return (
+    <div className="font-bold text-6xl">
+      custom App
+      <Hello />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Hello.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hello = () => {
+  return <div>hello</div>;
+};
+
+export default Hello;
